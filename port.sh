5ca6ddd1fce76774688dc5a96103e4614ca2962e
Zachery Hostens <zacheryph@gmail.com>
2017-07-20 02:07:39 -0500

    add port.sh. the container management script

--- port.sh
+++ port.sh
@@ -2,6 +2,8 @@
 
 PORTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+USERNAME=$(docker system info 2>/dev/null | grep "^Username:" | sed 's|^Username: *||')
+
 read -r -d '' USAGE <<EOF
 port.sh: container port management
 
@@ -20,17 +22,38 @@ usage() {
   exit
 }
 
+ensure_app() {
+  app="$1"
+
+  if [[ -z "$app" ]] || [[ ! -d "$app" ]]; then
+    echo "!! $app not found or not given"
+    exit
+  fi
+}
+
+pull_image_id() {
+  name="$1"
+  tag=${2:-latest}
+
+  docker images -q "${USERNAME}/${name}:${tag}"
+}
+
 port_build() {
-  # need to get Username from: docker system info
-  echo "not implemented"
+  app=$1
+  ensure_app "$app"
+
+  docker build -t "${USERNAME}/${app}" "$app"
 }
 
 port_install() {
-  for runner in $(find $PORTDIR -type f -name run -not -wholename '*/template/run'); do
+  find "$PORTDIR" -type f -name run -not -wholename "*/template/run" |
+  while read -r runner; do
     app=$(basename $(dirname $runner))
     echo "== linking ${app}"
     ln -sf $runner ${HOME}/bin/${app}
   done
+
+  # TODO: remove stubs that no longer exist
 }
 
 port_new() {
@@ -40,24 +63,59 @@ port_new() {
   fi
 
   mkdir $app
+
   for f in $(ls template); do
     cat template/$f \
       | sed "s|__NAME__|$app|" \
       > $app/$f
 
-    chmod --reference=template/$f $app/$f
+    chmod --reference="template/$f" "$app/$f"
   done
 
   echo "== created $app for editing"
 }
 
 port_push() {
-  echo "not implemented"
+  app="$1"
+  ensure_app "$app"
+  id=$(pull_image_id "${app}")
+  if [[ -z "$id" ]]; then
+    echo "!! image not found for ${app}"
+    exit
+  fi
+
+  docker images --format '{{.ID}} {{.Tag}}' "${USERNAME}/${app}" | \
+    grep "^${id}" | \
+    awk '{{print $2}}' | \
+    while read -r tag; do
+      echo "== pushing ${app}:${tag}"
+      docker push "${USERNAME}/${app}:${tag}"
+    done
 }
 
 port_tag() {
-  # run latest with --version, docker tag
-  echo "not implemented"
+  app=$1
+  ensure_app "$app"
+
+  id_latest=$(pull_image_id "${app}")
+  if [[ -z "$id_latest" ]]; then
+    echo "!! need to build image first"
+    exit
+  fi
+
+  if ! version=$(docker run --rm "${USERNAME}/${app}" --version); then
+    echo "!! failed to retrieve version with --version. must manually tag"
+    exit
+  fi
+
+  id_version=$(pull_image_id "${app}" "${version}")
+  if [[ -n "$id_version" ]]; then
+    echo "!! ${app}:${version} already tagged"
+    exit
+  fi
+
+  echo "== tagging ${USERNAME}/${app}:${version} => ${id_latest}"
+  docker tag "${USERNAME}/${app}:latest" "${USERNAME}/${app}:${version}"
 }
 
 cmd=$1 ; shift
